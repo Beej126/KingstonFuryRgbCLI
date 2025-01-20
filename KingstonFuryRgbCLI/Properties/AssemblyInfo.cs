eea6da3a3bf64f3250f91470a25a1ea182ab6c7b
Beej <6301228+Beej126@users.noreply.github.com>
2025-01-19 20:15:53 -0800

    Create build-release.yml

--- KingstonFuryRgbCLI/Properties/AssemblyInfo.cs
+++ KingstonFuryRgbCLI/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.Versioning;
 
 [assembly: AssemblyTitle("KingstonFuryRgbCLI")]
 [assembly: AssemblyProduct("KingstonFuryRgbCLI")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 [assembly: AssemblyDescription("Based on decompile of FuryControllerService v2.0.0.42")]
 //[assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyCompany("")]
@@ -14,3 +14,4 @@ using System.Runtime.Versioning;
 //[assembly: AssemblyTrademark("")]
 //[assembly: ComVisible(false)]
 [assembly: Guid("eea8b047-7b0c-4f97-871c-fe00a3cbb929")]
+[assembly: AssemblyInformationalVersion("0.0.1-7+Branch.main.Sha.564e7d4f48325d1ed473f5136398d87dbd94a5f3")]
